7419892f0a1e240560c126869417b74f8bdbf205
Mark Benjamin Puerto <krampuerto@gmail.com>
2022-03-04 15:03:24 +0800

    Refactor code to add on concern
    Signed-off-by: Mark Benjamin Puerto <krampuerto@gmail.com>

--- app/poros/task_5.rb
+++ app/poros/task_5.rb
@@ -1,15 +1,6 @@
 class Task5 < ActiveRecord::Base
+  include Noteable
+
   has_many :notes, as: :noteable, dependent: :destroy
 
-  def has_simple_notes?
-    notes.not_reminders_or_todos.any?
-  end
-
-  def has_to_do_notes?
-    notes.to_dos.any?
-  end
-
-  def has_reminder_notes?
-    notes.reminders.any?
-  end
 end
